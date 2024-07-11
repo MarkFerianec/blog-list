4c5b2f2ea0fb62722acc572201d25bf29f185528
Mark Ferianec <mjf397@gmail.com>
2024-07-11 13:14:02 -0600

    Finish exercise 4.2

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-const blogsRouter = require('./controllers/blogs');
+const app = require('./app'); // The Express app
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-
-mongoose.connect(config.MONGODB_URI);
-
-app.use(middleware.morgan('tiny'));
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
