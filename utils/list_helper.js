b347d0c831ece3f3498a0f114f5ed80ed111779a
Mark Ferianec <mjf397@gmail.com>
2024-07-14 12:44:47 -0600

    Finish exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  total = 0;
+
+  if (blogs.length === 0) {
+    return 0;
+  } else if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else {
+    blogs.forEach((blog) => {
+      total += blog.likes;
+    });
+    return total;
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
