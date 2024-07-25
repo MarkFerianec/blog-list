5467dab7c787af8dfa86f4577fafc995e71ac4b7
Mark Ferianec <mjf397@gmail.com>
2024-07-24 23:38:46 -0600

    Finish exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -66,6 +66,20 @@ test('verify that making an HTTP POST request to the /api/blogs URL successfully
   assert(contents.includes('title of new blog'));
 });
 
+test('delete a blog and return a status code of 204', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+  const titles = blogsAtEnd.map((blog) => blog.title);
+  assert(!titles.includes(blogToDelete.title));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
